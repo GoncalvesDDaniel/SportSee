0cc96f09d921f1016dd56b745f6ea124988bd4b9
Daniel D Goncalves <107401263+GoncalvesDDaniel@users.noreply.github.com>
2025-03-10 18:49:20 +0100

    Fix json on ActivityCharts.jsx

--- src/components/charts/ActivityCharts.jsx
+++ src/components/charts/ActivityCharts.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import mockedActivityData from "./../../services/mocks/mockedActivityData.json";
+import id1 from "./../../services/mocks/user/id1.json";
+import activity from "./../../services/mocks/user/1/activity.json";
+import averageSessions from "./../../services/mocks/user/1/average-sessions.json";
+import performance from "./../../services/mocks/user/1/performance.json";
 import "./../../assets/styles/components/charts/_activityCharts.scss";
 import {
     Bar,
@@ -11,19 +14,29 @@ import {
     XAxis,
     YAxis,
 } from "recharts";
+import { useParams } from "react-router";
 
 function ActivityCharts() {
+    // let params = useParams();
+    // console.log(params.id);
+    console.log(activity.data.sessions);
+
     return (
         <div className="activityCharts-container">
             <ResponsiveContainer aspect={835 / 320}>
-                <BarChart width="100%" height="100%" data={mockedActivityData}>
+                <BarChart
+                    width="100%"
+                    height="100%"
+                    data={activity.data.sessions}
+                    barCategoryGap={8}
+                >
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
+                    <XAxis dataKey="day" />
+                    <YAxis dataKey="kilogram" />
                     <Tooltip />
                     <Legend />
-                    <Bar dataKey="pv" fill="#8884d8" />
-                    <Bar dataKey="uv" fill="#82ca9d" />
+                    <Bar dataKey="kilogram" fill="#000" />
+                    <Bar dataKey="calories" fill="#E60000" />
                 </BarChart>
             </ResponsiveContainer>
         </div>

b8471e5a14be8bbb20d93ad3c9e49fdf06d56d25
Daniel D Goncalves <107401263+GoncalvesDDaniel@users.noreply.github.com>
2025-04-07 17:54:41 +0200

    Fix responsive on 1024px

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { NavLink, Outlet, UNSAFE_RemixErrorBoundary } from "react-router";
 import "./App.css";
-import Dashboard from "./app/pages/Dashboard";
-import FetchData from "./services/api/useFetch";
 import DashboardLayout from "./app/layouts/DashboardLayout";
-import { Suspense } from "react";
 
 function App() {
-    alert("hello App");
-    return (
-        // <Suspense>
-        <DashboardLayout />
-        // </Suspense>
-    );
+    return <DashboardLayout />;
 }
 
 export default App;
